d5d617697cac26fd09030b4248633fcd861cec65
Alexey Khokhryakov <alexey.khokhryakov@gmail.com>
2016-10-31 18:25:30 +0300

    + 004 solution

---
+++ src/004.js
@@ -0,0 +1,24 @@
+export default () => ((digits) => {
+    let
+        products = function* (digits) {
+            let
+                max = Math.pow(10, digits) - 1,
+                min = Math.pow(10, digits - 1);
+
+            for (let i = 0; i <= max; i++) {
+                for (let j = Math.ceil(i / 2); j <= i; j++) {
+                    if (max - j > min && max - (i - j) > min) {
+                        yield (max - j) * (max - (i - j));
+                    }
+                }
+            }
+        },
+        str = (n) => n + '',
+        isPalindrome = (s) => str(s) === str(s).split('').reverse().join('');
+
+    for (let product of products(digits)) {
+        if (isPalindrome(product)) {
+            return product;
+        }
+    }
+})(3);
--- src/list.js
+++ src/list.js
@@ -1,3 +1,4 @@
 export { default as p001 } from './001.js';
 export { default as p002 } from './002.js';
 export { default as p003 } from './003.js';
+export { default as p004 } from './004.js';
